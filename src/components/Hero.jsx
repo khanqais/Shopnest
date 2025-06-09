bb1bdff4baa514acdf4892de839cc0e78b7b00e5
ayaz1017 <your-email@example.com>
2025-06-09 19:46:12 +0530

    New Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className="hero-section">
       <div className="hero-left">
         <p className="subtitle">— OUR BESTSELLERS</p>
-        <h1 className="title">Latest Arrivals</h1>
+        <h1 className="prata-regular">Latest Arrivals</h1>
         <p className="shop-link">SHOP NOW</p>
       </div>
       <div className="hero-right">
