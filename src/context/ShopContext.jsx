f1245e289ad93204c23affac9d68f1e0adc65089
ayaz1017 <your-email@example.com>
2025-06-11 17:53:21 +0530

    product aur product ke id tak

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,14 +1,42 @@
-import { createContext } from "react";
+import { createContext, useState,useEffect } from "react";
 import { products } from "../assets/assets";
 export const ShopContext = createContext();
 
 const ShopContextProvider=(props)=>{
     const currency = '$';
     const delivery_free=10
+    const [cartitem,setCartitem]=useState({});
+    
+    const AddtoCart=async (itemid,size)=>{
+        let CartData=structuredClone(cartitem);
 
+        if(CartData[itemid])
+        {
+            if(CartData[itemid][size])
+            {
+                CartData[itemid][size]+=1
+            }
+            else{
+                CartData[itemid][size]=1;
+            }
+        }
+        else{
+            CartData[itemid]={};
+            CartData[itemid][size]=1;
+        }
+        setCartitem(CartData); 
+    }
+    useEffect(() => {
+      
+    
+       console.log(cartitem);
+       
+    }, [cartitem])
+    
 
     const value={
-      products  , currency ,delivery_free
+      products  , currency ,delivery_free,
+      cartitem,AddtoCart
     }
     return (
         <ShopContext.Provider value={value}>
